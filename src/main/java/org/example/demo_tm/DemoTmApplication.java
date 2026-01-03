6a80013b711dfe912a5b629fc808a2e27c4c0a78
Sagcyber <immortelleame@gmail.com>
2026-01-03 17:37:10 +0700

    initial commit

---
+++ src/main/java/org/example/demo_tm/DemoTmApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo_tm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTmApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTmApplication.class, args);
+    }
+    
+}
