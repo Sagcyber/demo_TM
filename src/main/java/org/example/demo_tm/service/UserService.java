23baca4bf10afbf93c0904a63ae222b9dee5dd90
Sagcyber <immortelleame@gmail.com>
2026-01-03 20:00:47 +0700

    some changes

--- src/main/java/org/example/demo_tm/service/UserService.java
+++ src/main/java/org/example/demo_tm/service/UserService.java
@@ -3,10 +3,12 @@ package org.example.demo_tm.service;
 import org.example.demo_tm.dto.CreateUserDto;
 import org.example.demo_tm.entity.User;
 import org.example.demo_tm.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
     
     private final UserRepository userRepository;
