f420b33745f1e547f507b0920910fae1ce921c31
Sagcyber <immortelleame@gmail.com>
2026-01-03 18:25:04 +0700

    add CreateUserDto class logic

--- src/main/java/org/example/demo_tm/dto/CreateUserDto.java
+++ src/main/java/org/example/demo_tm/dto/CreateUserDto.java
@@ -1,5 +1,21 @@
 package org.example.demo_tm.dto;
 
 public class CreateUserDto {
+    
+    private String name;
+    
+    public CreateUserDto() {}
+    
+    public CreateUserDto(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
